21e620c6d7671fc89471271dca3f79f99bd4c6af
etecnos <90986423+etecnos@users.noreply.github.com>
2024-10-17 11:34:44 +0300

    main.py

--- main.py
+++ main.py
@@ -6,7 +6,6 @@ from merge import execute
 from detect import detect, give
 from tables import tables
 
-
 # The Libraries we are going to use
 import pygame
 import sys
@@ -19,21 +18,17 @@ pygame.init()
 SCREEN_WIDTH, SCREEN_HEIGHT = 800, 600
 WHITE = (255, 255, 255)
 
-
-pygame_icon= pygame.image.load('data/icon.png')
+pygame_icon = pygame.image.load('data/icon.png')
 pygame.display.set_icon(pygame_icon)
-
 
 # func variable changes according to which action is selected
 func = 0
 # Start variable helps inside the running loop
 start = True
 
-
 file = ""
 times = 0
 flag_tables = True
-
 
 # Create the screen object
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
@@ -57,19 +52,18 @@ clicked_image_index = -1
 
 
 # call_text function is used when we want to display text in pygame's screen
-def call_text(tx,wdt):
+def call_text(tx, wdt):
     screen.fill(WHITE)
     font = pygame.font.Font(None, 36)
     text = font.render(tx, True, (0, 0, 0))
     screen.blit(text, (SCREEN_WIDTH // 2 - wdt, 200))
 
+
 # call_text_lines use is the same with the previous function, but it is used if only a specific action is completed(you can see it below)
 def call_text_lines(tx):
     font = pygame.font.Font(None, 28)
     text = font.render(tx, True, (0, 0, 0))
-    screen.blit(text, (SCREEN_WIDTH // 50 , 250))
-
-
+    screen.blit(text, (SCREEN_WIDTH // 50, 250))
 
 
 # this function checks whether any of the given images is selected(image = action)
@@ -79,11 +73,9 @@ def check_image_click(pos):
         rect = pygame.Rect(img_pos[0], img_pos[1], image.get_width(), image.get_height())
         if rect.collidepoint(pos):
             # The variable below is used to hold the value of the selected action
-            clicked_image_index = idx + 1  
-            
+            clicked_image_index = idx + 1
 
-
-#Again loop is used for the program to restart after performing an action
+# Again loop is used for the program to restart after performing an action
 again = True
 while again:
 
@@ -94,33 +86,33 @@ while again:
 
         # Draw the images
         if start:
-            call_text("Select an action:",100)
+            call_text("Select an action:", 100)
             for image, pos in zip(images, image_positions):
                 screen.blit(image, pos)
         # This if is checking whether an action has ended(start = false). In this case the right text is displayed
         if not start:
             if func == 1:
-                call_text("PDF is created!",100)
+                call_text("PDF is created!", 100)
             if func == 2:
-                call_text("PDF files are created",120)
+                call_text("PDF files are created", 120)
             if func == 3:
-                call_text("Files successfully merged(directory of the program)",310)
+                call_text("Files successfully merged(directory of the program)", 310)
             if func == 4:
                 call_text("This word was detected " + str(times) + " times inside the PDF file", 300)
                 if times > 0:
-                    call_text_lines("Find the exact lines the word was detected inside lines.txt(directory of the program)")
+                    call_text_lines(
+                        "Find the exact lines the word was detected inside lines.txt(directory of the program)")
             if func == 5:
                 if flag_tables == True:
-                    call_text("No tables found!",100)
+                    call_text("No tables found!", 100)
                 else:
-                    call_text("Tables are extracted inside tables.txt(directory of the program)",370)        
+                    call_text("Tables are extracted inside tables.txt(directory of the program)", 370)
             pygame.display.update()
             # Wait 5 seconds and then restart the program
             time.sleep(5)
             running = True
             start = True
             break
-
 
         # Event handling
         for event in pygame.event.get():
@@ -131,24 +123,19 @@ while again:
                 mouse_pos = pygame.mouse.get_pos()
                 check_image_click(mouse_pos)
 
-
-
-
         # Case one(image 1)
         if clicked_image_index == 1:
             func = 1
             call_text("Select an Image", 100)
             pygame.display.flip()
             photo()
-            clicked_image_index =-1
+            clicked_image_index = -1
             start = False
-
-
 
         # Case two(image 2)
         if clicked_image_index == 2:
             func = 2
-            call_text("Select a PDF file",90)
+            call_text("Select a PDF file", 90)
             pygame.display.flip()
             split()
             clicked_image_index = -1
@@ -159,7 +146,6 @@ while again:
             func = 3
             width, height = 800, 600
             screen = pygame.display.set_mode((width, height))
-
 
             # Define colors
             WHITE = (255, 255, 255)
@@ -172,7 +158,6 @@ while again:
             # Instructions and variables
             input_text = ''
             instruction_text = "How many files do you want to merge ?:"
-
 
             running = True
             enter_pressed = False
@@ -203,7 +188,6 @@ while again:
                 input_surface = font.render(input_text, True, BLACK)
                 screen.blit(input_surface, (20, 100))
 
-
                 if enter_pressed:
                     if input_text.isdigit():
                         files_list = []
@@ -214,7 +198,7 @@ while again:
                         # Handle file inputs
                         for i in range(int(input_text)):
                             screen.fill(WHITE)
-                            message = 'Give file ' +  str(i + 1)
+                            message = 'Give file ' + str(i + 1)
                             surface = font.render(message, True, BLACK)
                             screen.blit(surface, (300, 200))
                             pygame.display.flip()
@@ -222,17 +206,14 @@ while again:
                             files_list.append(merge(i))
                         execute(files_list)
                         clicked_image_index = -1
-                        input_text =""
+                        input_text = ""
                         start = False
                         break
 
                     else:
                         clicked_image_index = -1
 
-
                 pygame.display.flip()
-
-
 
         # Case four(image 4)
         if clicked_image_index == 4:
@@ -285,7 +266,6 @@ while again:
                 input_surface = font.render(input_text, True, BLACK)
                 screen.blit(input_surface, (20, 100))
 
-
                 if enter_pressed:
                     if not input_text.isdigit():
                         files_list = []
@@ -302,9 +282,6 @@ while again:
             clicked_image_index = -1
             start = False
 
-
-
-
         # Case five(image 5)
         if clicked_image_index == 5:
             func = 5
@@ -318,7 +295,6 @@ while again:
         pygame.display.flip()
     # Clean up Pygame
 pygame.quit()
-
 
 
 
